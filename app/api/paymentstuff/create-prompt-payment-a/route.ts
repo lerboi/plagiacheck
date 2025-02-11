1135d0951c320b990cacb5137071ff450a97ba97
lerboi <leroyzzng@gmail.com>
2025-02-12 04:58:29 +0800

    fixed price

--- app/api/paymentstuff/create-prompt-payment-a/route.ts
+++ app/api/paymentstuff/create-prompt-payment-a/route.ts
@@ -63,7 +63,7 @@ export async function GET(req: Request) {
                     product_data: {
                         name: "One-Time Purchase",
                     },
-                    unit_amount: parseInt(price) * 100, // Convert price to cents
+                    unit_amount: parseFloat(price), 
                 },
                 quantity: 1,
             }],
